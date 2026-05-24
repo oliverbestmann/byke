6e278535a9451128ee6ab1512c3ad2f63383a1b9
Oliver Bestmann <oliver.bestmann@googlemail.com>
2026-05-24 10:51:26 +0200

    byke2d: add a Path type to build 2d mesh instances

---
+++ byke2d/examples/path/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	. "github.com/oliverbestmann/byke"
+	. "github.com/oliverbestmann/byke/byke2d"
+	"github.com/oliverbestmann/byke/byke2d/glm"
+)
+
+func init() {
+	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	})
+
+	slog.SetDefault(slog.New(handler))
+}
+
+func main() {
+	var app App
+
+	app.AddPlugin(RenderPlugin)
+	app.AddSystems(Update, ExitOnEscapeSystem)
+	app.AddSystems(Startup, setupSystem)
+	app.MustRun()
+}
+
+func setupSystem(commands *Commands, assets *Assets) {
+	commands.Spawn(
+		Camera{},
+		TransformFromXYZ(0, 0, -0.5),
+		OrthographicProjection{
+			ViewportOrigin: glm.Vec2f{0.5, 0.5},
+			ScalingMode:    ScalingModeFixedHorizontal{ViewportWidth: 512},
+			// ScalingMode: ScalingModeWindowSize{},
+			Scale: 1.0,
+		},
+	)
+
+	var path Path
+	path.MoveTo(glm.Vec2f{-32, -32})
+	path.LineTo(glm.Vec2f{-32, 32})
+	path.LineTo(glm.Vec2f{16, 32})
+	path.QuadCurveTo(glm.Vec2f{32, 32}, glm.Vec2f{32, 16})
+	path.LineTo(glm.Vec2f{32, -32})
+	path.LineTo(glm.Vec2f{32, -32})
+	path.Close()
+
+	commands.Spawn(
+		TransformFromXYZ(0, 0, 0.1),
+		Mesh2d{Mesh: path.ToMesh(0.1)},
+		ColorMaterial{
+			Tint: ColorSRGBA(1.0, 0.0, 0.5, 1.0),
+		},
+	)
+}
--- byke2d/mesh2d-pipeline.go
+++ byke2d/mesh2d-pipeline.go
@@ -94,7 +94,7 @@ func (m mesh2dPipelineConfig) Specialize(ctx PipelineContext) RenderPipelineDesc
 			Buffers:    buffers,
 		},
 		Primitive: wgpu.PrimitiveState{
-			Topology: wgpu.PrimitiveTopologyTriangleList,
+			Topology: wgpu.PrimitiveTopologyLineStrip,
 			CullMode: wgpu.CullModeNone,
 		},
 		Multisample: multisampleState(m.SampleCount),
